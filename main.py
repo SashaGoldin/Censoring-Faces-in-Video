ffb23be66e8ed0700b3ab347c3be3b5190c9089d
SashaGoldin <97263426+SashaGoldin@users.noreply.github.com>
2024-06-10 15:06:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import cv2
+video = cv2.VideoCapture("smile.mp4")
+
+success, frame = video.read()
+height = frame.shape[0]
+width = frame.shape[1]
+face_cascade = cv2.CascadeClassifier("faces.xml")
+
+output = cv2.VideoWriter('blurred_smile.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, (width, height))
+
+while success:
+  faces = face_cascade.detectMultiScale(frame, 1.1, 4)
+  for (x, y, w, h) in faces:
+    frame[y:y+h,x:x+w] = cv2.blur(frame[y:y+h, x:x+w], (50, 50))
+  output.write(frame)
+  success, frame = video.read()
+output.release()
